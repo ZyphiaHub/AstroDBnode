7578d0707f3c20dfd6c27675e1e9ee4cf552d31f
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2025-02-15 11:29:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Database = require('better-sqlite3');
 const app = express();
 const db = new Database('./game_data.db');
-const row = db.prepare("SELECT * FROM potions").get(1);
+const row = db.prepare("SELECT * FROM potions WHERE id = ?").get(1);
 console.log(row);
 
 // Middleware a JSON adatok kezeléséhez
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // API végpont az adatok lekérdezéséhez
 app.get('/api/data', (req, res) => {
-    db.all('SELECT * FROM mytable', (err, rows) => {
+    db.all('SELECT * FROM potions', (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
         } else {
@@ -22,7 +22,7 @@ app.get('/api/data', (req, res) => {
 // API végpont az adatok beszúrásához
 app.post('/api/data', (req, res) => {
     const { column1, column2 } = req.body;
-    db.run('INSERT INTO mytable (column1, column2) VALUES (?, ?)', [column1, column2], function(err) {
+    db.run('INSERT INTO potions (column1, column2) VALUES (?, ?)', [column1, column2], function(err) {
         if (err) {
             res.status(500).json({ error: err.message });
         } else {
